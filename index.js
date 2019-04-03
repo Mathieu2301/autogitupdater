3a6d010a1dce2743f4a4b4bcb72e0c93dcf4c852
Mathieu Colmon <mathieu@usp-3.fr>
2019-04-03 18:28:48 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var shell = require('shelljs');
-
-module.exports = function(url, app){
-    app.get(url, function(req, res){
-        res.send("RELOADING APP");
-        shell.exec('git pull');
-    })
-}
